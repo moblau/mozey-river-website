1ebf4e578b9614b056056e9181a5612d9b38e2d8
moblau <blausteinmorris@gmail.com>
2024-05-21 14:19:50 -0700

    init

---
+++ js/p5background.js
@@ -0,0 +1,132 @@
+let systems = [];
+
+function setup() {
+    let canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('p5-background');
+    
+    // Create multiple solar systems
+    for (let i = 0; i < 10; i++) {
+        systems.push(new SolarSystem(random(width), random(height)));
+    }
+}
+
+function draw() {
+    drawStarrySky(); // Draw starry sky background
+    
+    for (let system of systems) {
+        system.update();
+        system.display();
+    }
+}
+
+function drawStarrySky() {
+    // Dark sky background
+    background(0);
+    
+    // Draw glowing stars in the distance
+    for (let i = 0; i < 200; i++) {
+        let x = random(width);
+        let y = random(height);
+        let size = random(1, 3);
+        let brightness = random(150, 255);
+        fill(255, brightness);
+        noStroke();
+        ellipse(x, y, size);
+    }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+class SolarSystem {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.dx = random(-1, 1);
+        this.dy = random(-1, 1);
+        this.planets = [];
+        
+        for (let i = 0; i < random(2, 5); i++) {
+            this.planets.push(new Planet(random(10, 20), random(TWO_PI), random(1, 3), random(30, 100)));
+        }
+    }
+    
+    update() {
+        // Update position
+        this.x += this.dx;
+        this.y += this.dy;
+        
+        // Bounce off edges
+        if (this.x < 0 || this.x > width) this.dx *= -1;
+        if (this.y < 0 || this.y > height) this.dy *= -1;
+        
+        // Update planets
+        for (let planet of this.planets) {
+            planet.update();
+        }
+    }
+    
+    display() {
+        // Draw central star
+        fill(255, 204, 0);
+        noStroke();
+        ellipse(this.x, this.y, 40);
+        
+        // Draw planets
+        for (let planet of this.planets) {
+            planet.display(this.x, this.y);
+        }
+    }
+}
+
+class Planet {
+    constructor(size, angle, speed, distance) {
+        this.size = size;
+        this.angle = angle;
+        this.speed = speed;
+        this.distance = distance;
+        this.trail = [];
+        
+        // Generate random gradient colors
+        this.color1 = color(random(255), random(255), random(255));
+        this.color2 = color(random(255), random(255), random(255));
+    }
+    
+    update() {
+        this.angle += this.speed * 0.01;
+        
+        // Add current position to trail
+        this.trail.push({ x: this.calculateX(), y: this.calculateY() });
+        
+        // Limit trail length
+        if (this.trail.length > 20) {
+            this.trail.shift(); // Remove oldest point
+        }
+    }
+    
+    display(cx, cy) {
+        // Draw trail
+        for (let i = 0; i < this.trail.length - 1; i++) {
+            let from = this.trail[i];
+            let to = this.trail[i + 1];
+            let gradient = lerpColor(this.color1, this.color2, i / this.trail.length);
+            strokeWeight(this.size / 10);
+            stroke(gradient);
+            line(from.x, from.y, to.x, to.y);
+        }
+        
+        // Draw current position
+        fill(this.color2);
+        noStroke();
+        ellipse(this.calculateX(cx), this.calculateY(cy), this.size);
+    }
+    
+    calculateX(cx) {
+        return cx + cos(this.angle) * this.distance;
+    }
+    
+    calculateY(cy) {
+        return cy + sin(this.angle) * this.distance;
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,71 @@
+// // Function to detect if the user is on a mobile device
+// function isMobileDevice() {
+//     return /Mobi|Android/i.test(navigator.userAgent);
+// }
+
+// // Redirect if not on a mobile device
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     if (!isMobileDevice()) {
+//         window.location.href = 'https://example.com/regular-website';
+//     }
+// });
+
+var songs = [
+    "airwaves.wav",
+    "caterpillar.wav",
+    "nosoap.wav",
+    "santa.wav",
+    "outro.wav"
+];
+
+var audio = new Audio(); 
+var currentTrack = document.getElementById("current-track");
+var isPlaying = false;
+var currentIndex = -1;
+var history = [];
+
+
+function playPause() {
+    if (isPlaying) {
+        audio.pause();
+        isPlaying = false;
+    } else {
+        audio.play();
+        isPlaying = true;
+    }
+}
+
+function skipSong() {
+    var nextSongIndex = Math.floor(Math.random() * songs.length);
+    audio.src = songs[nextSongIndex];
+    currentTrack.innerText = "Now Playing: " + songs[nextSongIndex];
+    audio.play();
+    isPlaying = true;
+    addToHistory(nextSongIndex);
+}
+function addToHistory(index) {
+    currentIndex++;
+    history[currentIndex] = index;
+}
+
+function previousSong() {
+    if (currentIndex > 0) {
+        currentIndex--;
+        audio.src = songs[history[currentIndex]];
+        currentTrack.innerText = "Now Playing: " + songs[history[currentIndex]];
+        audio.play();
+        isPlaying = true;
+    }
+}
+
+audio.onplay = function() {
+    isPlaying = true;
+};
+
+audio.onpause = function() {
+    isPlaying = false;
+};
+
+audio.onended = function() {
+    skipSong(); // Automatically play the next song when the current one ends
+};
